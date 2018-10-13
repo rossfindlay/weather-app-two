0200c88d7d77807ad147013605af01b8b9bf934c
rossfindlay <ross.findlay@live.co.uk>
2018-10-13 11:39:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+
+    this.getLatLng = this.getLatLng.bind(this)
+  }
+
+
+
+  getLatLng() {
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=Edinburgh&key=${process.env.REACT_APP_GOOGLE_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
+
+
   render() {
+    this.getLatLng()
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
